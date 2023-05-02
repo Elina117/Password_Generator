55f773563dbe2b58db43903f5f923fb28c44d0c9
Elina117 <106472257+Elina117@users.noreply.github.com>
2023-05-02 22:53:11 +0300

    i need to write tests and finish project

---
+++ MyTestClass/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyTestClass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyTestClass")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e7eedc7-112d-42d0-9c1c-6340f7bf1b54")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyTestClass/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection.Emit;
+using Password_Generator;
+
+namespace MyTestClass
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        
+        public void TestPictureBox3Click1()
+        {
+            // Arrange
+            Form1 form = new Form1();
+            form.label_length_of_password.Text = "6";
+
+            // Act
+            form.pictureBox3_Click_1(null, null);
+            int newLength = int.Parse(form.label_length_of_password.Text);
+
+            // Assert
+            Assert.IsTrue(newLength >= 8 && newLength <= 16, "The password length should be between 8 and 16");
+        }
+    }
+}
